f0e666ba90f555fffc5dcba5e6b1bbc3f457785a
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-06-23 16:36:50 +0000

    b1

--- c++/j.java
+++ c++/j.java
@@ -1,6 +1,9 @@
 class j{
     public static void main(String[]args) {
         System.out.println("hellow java");
+        System.out.println("hellow java");
+        System.out.println("hellow java");
+        System.out.println("hellow java");
         
     }
 }
