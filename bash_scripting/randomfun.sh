ac1ab8ec526704f58b2a732b211970e6e07e5f0f
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-10-14 13:50:33 +0000

    day 3 prac done

---
+++ bash_scripting/randomfun.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+chmod +x randomfun.sh
+echo "u wan to do it ? 😏"
+read fun
+echo "ls -> $(ls)"
+echo "pwd-> $PWD"
+echo "random :) $RANDOM"
+echo "absh is using -> $SHELL"
+echo "Hostname -> $HOSTNAME"
+sleep 2
+echo "ip -> $(ip)"
+echo "user -> $USER"
+
